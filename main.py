9d2f22008fc1f7f453d27796baaa17bfae64ced1
Qlbbl1111 <quinnlburke@icloud.com>
2022-03-16 10:19:30 -0700

    Updated Database Format

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import json
 import flask
 from flask_restful import Resource, Api, reqparse
 import random
 from flask import request, jsonify
-from gifs_database import gifs
 
 
 app = flask.Flask(__name__)
 #app.config["DEBUG"] = True
 api = Api(app)
+gifs_database = open("gifs.json")
+
+gifs = json.loads(gifs_database.read())
 
 @app.route('/', methods=['GET'])
 def home():
@@ -15,12 +18,13 @@ def home():
 <p>A silly API for fetching gifs and images I've uploaded from Hazbin Hotel/Helluva Boss shows.</p>'''
 
 
-@app.route('/api/v1/hazbin/all', methods=['GET'])
+@app.route('/v1/hazbin/gif/all', methods=['GET'])
 def api_all():
+
     return jsonify(gifs)
 
 
-@app.route('/api/v1/hazbin/gif/random', methods=['GET'])
+@app.route('/v1/hazbin/gif/random', methods=['GET'])
 def api_id():
 
     return jsonify(gifs[random.randint(0, 3)])
